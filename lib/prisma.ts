589d90db46f112ba51878fce14113282ea02b4e3
urijv24 <urieljunca@hotmail.com>
2026-05-16 11:22:41 -0500

    fix: corregir import de PrismaClient para Prisma 7

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
